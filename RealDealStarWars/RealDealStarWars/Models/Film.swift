34553c23d674f441b5cd3032bfdc2914190b22ad
Blake Kvarfordt <blakekvarfordt@gmail.com>
2019-10-02 21:23:12 -0600

    sets up Film model :

---
+++ RealDealStarWars/RealDealStarWars/Models/Film.swift
@@ -0,0 +1,13 @@
+//
+//  Film.swift
+//  RealDealStarWars
+//
+//  Created by Blake kvarfordt on 10/2/19.
+//  Copyright © 2019 Blake kvarfordt. All rights reserved.
+//
+
+import Foundation
+
+struct Film: Codable {
+    let title: String
+}
